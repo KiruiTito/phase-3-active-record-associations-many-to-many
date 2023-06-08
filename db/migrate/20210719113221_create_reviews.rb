50c94eb4e03fcb919da1aeb7237497db0227aeb9
KiruiTito <titokirui402@gmail.com>
2023-06-08 20:41:12 +0300

    Completed

--- db/migrate/20210719113221_create_reviews.rb
+++ db/migrate/20210719113221_create_reviews.rb
@@ -1,9 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-      t.integer :score
-      t.string :comment
-      t.integer :game_id # this is our foreign key
+      t.integer :game_id
+      t.integer :user_id
       t.timestamps
     end
   end
